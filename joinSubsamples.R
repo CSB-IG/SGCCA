a8a814f1478cad967ce1bb05b955f3f9b78d0201
mSolEdad <36210371+mSolEdadO@users.noreply.github.com>
2022-10-28 11:38:13 -0500

    Add files via upload

---
+++ joinSubsamples.R
@@ -0,0 +1,19 @@
+#!/usr/bin/env Rscript
+library(tidyverse)
+#library(irr)
+
+subtype=commandArgs(trailingOnly=TRUE)
+files=list.files()
+files=files[grep(subtype,files)]
+subsa=lapply(files,read_tsv)
+final=read_tsv(paste("../",subtype,".selected",sep=''))
+
+subsa=lapply(subsa,function(x) x%>%dplyr::count(feature))
+subsa=reduce(subsa,full_join,by="feature")
+colnames(subsa)[2:101]=paste("r",1:100,sep="")
+final=final%>%count(variable)
+colnames(final)[1]="feature"
+subsa=merge(final,subsa,by="feature",all.x=T)
+
+write_tsv(subsa,paste(subtype,"subsampled",sep='.'))
+#temp=apply(subsa[,2:101],1,table)
